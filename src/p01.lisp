13bfc5005a71a2e021b6ab43ea91d370aa77d7a3
Rune Nielsen <runenielsen@runbox.com>
2020-12-28 20:01:39 +0100

    adds sln 1

---
+++ src/p01.lisp
@@ -0,0 +1,14 @@
+;; (*) Find the last box of a list.
+;; Example:
+;; * (my-last '(a b c d))
+;; (D)
+
+;; Using recursion
+(defun my-last (list)
+  (cond
+    ((endp (cdr list)) list)
+    (t (my-last (cdr list)))))
+
+;; Simple
+(defun my-last (list)
+  (last list))
