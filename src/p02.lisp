bdab8bf3b0447a6f315a0927bd905d665754c981
Rune Nielsen <runenielsen@runbox.com>
2020-12-28 20:32:48 +0100

    adds sln 2

---
+++ src/p02.lisp
@@ -0,0 +1,14 @@
+;; (*) Find the last but one box of a list.
+;; Example:
+;; * (my-but-last '(a b c d))
+;; (C D)
+
+;; Using recursion
+(defun my-but-last (list)
+  (cond
+    ((endp (cdr (cdr list))) list)
+    (t (my-but-last (cdr list)))))
+
+;; Simple
+(defun my-but-last (list)
+  (last list 2))
